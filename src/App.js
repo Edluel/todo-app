1c44a4f00b55cf0d56e6f0d8872b914a524d6bcf
Edluel <edluel_machado@hotmail.com>
2023-03-17 22:31:07 -0300

    change style structure to sass and creating theme state on app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
 import Header from './components/header/Header';
+import index from './styles/index.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [theme, setTheme] = useState('dark');
+
+
   return (
     <div className="App">
-      < Header />
+      < Header theme={theme} setTheme={setTheme} />
     </div>
   );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useState } from 'react';
 
-export default function Header() {
+export default function Header(props) {
+  const {theme, setTheme} = props;
+
+  
+
   return (
     <div className="header">
         <h1>T O D O</h1>
