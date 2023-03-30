37c4bcd5e76c9aa31c260f120dd20c68b5fa065e
Edluel <edluel_machado@hotmail.com>
2023-03-29 21:43:49 -0300

    Feat: local storage to theme and task list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './styles/index.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Header from './components/header/Header';
 import AddToDo from './components/addToDo/AddToDo';
 import ToDoList from './components/toDoList/ToDoList';
 
-
 function App() {
-  const [theme, setTheme] = useState('dark');
-
-  const [data, setData] = useState([
+  const defaultData = [
     {
       index: 0,
       text: 'Fazer compras',
@@ -27,22 +24,43 @@ function App() {
       done: false,
       hidden: false
     },
-  ]);
+  ];
 
-
-  if (theme === 'dark') {
-    document.body.classList.add('dark');
-    document.body.classList.remove('light');
-  } else {
-    document.body.classList.remove('dark');
-    document.body.classList.add('light');
+  const getDefaultTheme = () => {
+    const storedTheme = localStorage.getItem('theme');
+    return storedTheme ? storedTheme : 'dark';
   }
+
+  const [theme, setTheme] = useState(getDefaultTheme);
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.body.classList.add('dark');
+      document.body.classList.remove('light');
+    } else {
+      document.body.classList.remove('dark');
+      document.body.classList.add('light');
+    }
+  }, [theme]);
+
+  const [data, setData] = useState(() => {
+    const storedData = localStorage.getItem('data');
+    return storedData ? JSON.parse(storedData) : defaultData;
+  });
+
+  useEffect(() => {
+    localStorage.setItem('data', JSON.stringify(data));
+  }, [data]);
 
   return (
     <div className="App">
-      < Header theme={theme} setTheme={setTheme} />
-      < AddToDo data={data} setData={setData} />
-      < ToDoList data={data} setData={setData} />
+      <Header theme={theme} setTheme={setTheme} />
+      <AddToDo data={data} setData={setData} />
+      <ToDoList data={data} setData={setData} />
     </div>
   );
 }
